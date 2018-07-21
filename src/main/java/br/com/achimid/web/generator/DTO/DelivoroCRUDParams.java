32f8885f4c227252ef3a51b537efafd051394add
Achimid <achimid@hotmail.com>
2018-07-21 09:05:59 -0300

    Addind input fields to add on template

--- src/main/java/br/com/achimid/web/generator/DTO/DelivoroCRUDParams.java
+++ src/main/java/br/com/achimid/web/generator/DTO/DelivoroCRUDParams.java
@@ -2,10 +2,12 @@ package br.com.achimid.web.generator.DTO;
 
 import br.com.achimid.web.generator.model.DelivoroCRUDConfig;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(callSuper=false)
 public class DelivoroCRUDParams extends DelivoroCRUDConfig {
 
     private String clazzNameFull;
